c973b31b1b3f2788cd45d637ef16a6422e8871fb
zjsxzy <zjsxzy@gmail.com>
2017-01-02 19:30:13 +0800

    update all

---
+++ dual moving average/main.py
@@ -0,0 +1,21 @@
+def init(context):
+    context.i = 0
+    context.security = "000402.XSHE"
+    context.hold = False
+
+def handle_bar(context, bar_dict):
+    context.i += 1
+    if context.i < 30:
+        return
+
+    security = context.security
+
+    short_mavg = history(10, '1d', 'close')[security].mean()
+    long_mavg = history(30, '1d', 'close')[security].mean()
+
+    if short_mavg > long_mavg and not context.hold:
+        order_target_percent(security, 1)
+        context.hold = True
+    elif short_mavg < long_mavg and context.hold:
+        order_target_percent(security, 0)
+        context.hold = False
